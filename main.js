426d6c8b81081c182a7f7e6be9e8fac010d31f76
NDUBA <user@users-MacBook-Pro.local>
2024-01-16 11:40:47 +0300

    Done

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
-// Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', function () {
+  const hearts = document.querySelectorAll('.like-glyph');
+  hearts.forEach(heart => {
+    heart.addEventListener('click', handleHeartClick);
+  });
+});
 
+function handleHeartClick(event) {
+  mimicServerCall()
+    .then(() => {
+      toggleHeart(event.target);
+    })
+    .catch(error => {
+      displayError(error);
+      setTimeout(() => hideError(), 3000);
+    });
+}
 
+function toggleHeart(heart) {
+  heart.classList.toggle('activated-heart');
+  if (heart.innerText === EMPTY_HEART) {
+    heart.innerText = FULL_HEART;
+  } else {
+    heart.innerText = EMPTY_HEART;
+  }
+}
 
+function displayError(error) {
+  const errorMessage = document.getElementById('modal-message');
+  errorMessage.innerText = error;
+  const errorModal = document.getElementById('modal');
+  errorModal.classList.remove('hidden');
+}
 
+function hideError() {
+  const errorModal = document.getElementById('modal');
+  errorModal.classList.add('hidden');
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
